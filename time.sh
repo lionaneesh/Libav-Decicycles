adae01202636023371e361f0df342c7b8f2bf23f
Aneesh Dogra <lionaneesh@gmail.com>
2012-05-08 07:51:18 +0530

    Output Minimum Decicycles.

--- time.sh
+++ time.sh
@@ -1,6 +1,6 @@
 LEAST_ARGS=3
 E_BADARGS=65
-MAX=10
+MAX=60
 count=1
 sum=0
 
@@ -11,14 +11,19 @@ then
   exit $E_BADARGS
 fi
 
+min=`$@ 2>&1 | grep -oh '[0-9]* decicycles' | grep -oh '[0-9]*' | tail -1`
 while [ $count -lt $MAX ]
 do
 	# We only care about the last line because it represents the most runs.
 	cycle=`$@ 2>&1 | grep -oh '[0-9]* decicycles' | grep -oh '[0-9]*' | tail -1`
+	if [ $cycle -lt $min ]
+	then
+		min=$cycle
+	fi
 	sum=$[$sum+$cycle]
 	count=$[$count+1]
 done
 
 let average=$sum/$count
-
 echo "Average Decicycles : $average"
+echo "Min     Decicycles : $min"
